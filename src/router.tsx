c277f9beb137dda13ef5f03683156b48f87df619
Vítor O <Caulicons@hotmail.com>
2023-05-01 20:06:10 -0300

    test08 - testing deploy

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import WrapperContent from './components/WrapperContent';
 import MainScreen from './pages/MainScreen';
-import SignUp from './pages/SignUp';
+import SignUp from './pages/SignUp/SignUp';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function RouterApp() {
